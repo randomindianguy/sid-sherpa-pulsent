6edf7fe5dca11b339e40816ba91a37bf2ee949ac
Sidharth Sundaram <sidharth.rama@gmail.com>
2026-05-04 20:31:16 -0400

    updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
 import { Hero } from "./components/Hero";
-import { AuditContext } from "./components/AuditContext";
-import { Layer2 } from "./components/Layer2";
-import { Findings } from "./components/Findings";
-import { Experiments } from "./components/Experiments";
+import { Pulsent } from "./components/Pulsent";
+import { Role } from "./components/Role";
 import { About } from "./components/About";
 
 export default function Home() {
   return (
     <main className="relative">
       <Hero />
-      <AuditContext />
-      <Layer2 />
-      <Findings />
-      <Experiments />
+      <Pulsent />
+      <Role />
       <About />
     </main>
   );
